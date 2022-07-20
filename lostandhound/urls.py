be44ba1678a616079da4fa8396758046f70b81f8
KELLY LUU <kellyluuu@gmail.com>
2022-07-19 23:14:39 -0500

    update urls.py

--- lostandhound/urls.py
+++ lostandhound/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main_app.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
