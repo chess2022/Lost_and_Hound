d6aadda4e01ef1e413eb28d31bae7683690a389c
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2022-07-19 20:59:19 -0700

    fixes pet model spelling of cat

--- main_app/models.py
+++ main_app/models.py
@@ -9,7 +9,7 @@ class Pet(models.Model):
   OTHER = 'OT'
   PET_TYPE = [
     (DOG, 'Dog'),
-    (CAT, 'CAT'),
+    (CAT, 'Cat'),
     (OTHER, 'Other')
   ]
   name = models.CharField(max_length=100, blank=False)
@@ -27,7 +27,7 @@ class Pet(models.Model):
 
   def __str__(self):
     return self.name
-    
+
   def get_absolute_url(self):
       return reverse("detail", kwargs={'pet_id':self.id})
   
