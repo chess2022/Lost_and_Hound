903277fdccda6ff300e7d8f451d62e791cf68e92
chess2022 <lakeshore.hnh@gmail.com>
2022-07-19 23:29:25 -0700

    Updates Pet model

--- main_app/models.py
+++ main_app/models.py
@@ -12,6 +12,7 @@ class Pet(models.Model):
     (CAT, 'Cat'),
     (OTHER, 'Other')
   ]
+  type = models.CharField(max_length=2, choices=PET_TYPE, default=DOG)
   name = models.CharField(max_length=100, blank=False)
   breed = models.CharField(max_length=100, blank=False)
   city = models.CharField(max_length=100, blank=False)
@@ -23,8 +24,16 @@ class Pet(models.Model):
     (MALE, 'Male'),
     (FEMALE, 'Female')
   ]
+  sex = models.CharField(max_length=2, choices=PET_SEX, default=MALE)
   comments = models.TextField(blank=True)
-
+  LOST = 'LT'
+  FOUND = 'FD'
+  STATUS_OPTIONS = [
+    (LOST, 'Lost'),
+    (FOUND, 'Found')
+  ]
+  status = models.CharField(max_length=2, choices=STATUS_OPTIONS, default=LOST)
+  
   def __str__(self):
     return self.name
 
