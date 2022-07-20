7c9d04271d38c3e8a63a3bf8526e29d698d80c2a
KELLY LUU <kellyluuu@gmail.com>
2022-07-20 00:37:33 -0500

    update user models

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from main_app.forms import signUpForm
 
 # Create your views here.
 
 # ------------------------- KL-todo apply auth routes ------------------------ #
-# from django.contrib.auth.decorators import login_required
-# from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 
@@ -19,19 +20,21 @@ def about(request):
     return render(request, 'about.html')
 
 def lostandhound_index(request):
-
-  return render(request, 'lostandhound/index.html')
+  return render(request, 'pet/index.html')
 
 def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      user = form.save()
-      login(request, user)
-      return redirect('index')
-    else:
-      error_message = 'Invalid sign up - try again'
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
+    error_message = ''
+    if request.method == 'POST':
+        form = signUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign-up. Please try again'
+    form = signUpForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
