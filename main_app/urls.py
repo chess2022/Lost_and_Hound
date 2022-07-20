5916be54db04975c4bc57aeb1bc075d4624ec704
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2022-07-20 16:55:53 -0700

    cleans up files and test for github desktop

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,9 +3,4 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  # path('pets/', views.pets_index, name='index'),
-  # path('pets/<int:pet_id>/', views.pets_detail, name='detail'),
-  # path('pets/create/', views.PetCreate.as_view(), name='pets_create'),
-  # path('pets/<int:pk>/update/', views.PetsUpdate.as_view(), name='pets_update'),
-  # path('pets/<int:pk>/delete/', views.PetDelete.as_views(), name='pets_delete'),
 ]
