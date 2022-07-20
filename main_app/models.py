5b4b5c50f9f45df22c40a2aee21b4abc0db26088
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2022-07-19 20:58:30 -0700

    adds pet model and home URL

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,36 @@
+from typing import TYPE_CHECKING
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+class Pet(models.Model):
+  DOG = 'DG'
+  CAT = 'CT'
+  OTHER = 'OT'
+  PET_TYPE = [
+    (DOG, 'Dog'),
+    (CAT, 'CAT'),
+    (OTHER, 'Other')
+  ]
+  name = models.CharField(max_length=100, blank=False)
+  breed = models.CharField(max_length=100, blank=False)
+  city = models.CharField(max_length=100, blank=False)
+  state = models.CharField(max_length=100, blank=False)
+  breed = models.CharField(max_length=100, blank=False)
+  MALE = 'ML'
+  FEMALE = 'FM'
+  PET_SEX = [
+    (MALE, 'Male'),
+    (FEMALE, 'Female')
+  ]
+  comments = models.TextField(blank=True)
+
+  def __str__(self):
+    return self.name
+    
+  def get_absolute_url(self):
+      return reverse("detail", kwargs={'pet_id':self.id})
+  
+
+  
+  
