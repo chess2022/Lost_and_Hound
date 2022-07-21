e79fe81a22326708da2f007f0e0945fac27cf4b0
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2022-07-20 17:57:30 -0700

    adds files cleanup

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from .models import Pet 
 
-# class PetForm(ModelForm):
-#   class Meta:
-#     model = Pet
-#     fields = ['PET_TYPE', 'PET_SEX']
+class PetForm(ModelForm):
+  class Meta:
+    model = Pet
+    fields = '__all__'
