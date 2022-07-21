246f0c93c35fab0a1fff1fdac913fb9f4940ecfe
chess2022 <lakeshore.hnh@gmail.com>
2022-07-21 04:54:42 -0700

    Adds basic create form and detail

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.views.generic import View
+from django.views.generic import View, ListView, DetailView
 from main_app.forms import signUpForm
 from django.contrib.auth import login, authenticate
 from .process import create_pdf 
@@ -8,16 +8,8 @@ from django.template.loader import render_to_string
 from main_app import models
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic import ListView, DetailView
 from .models import Pet, Photo
 from .forms import PetForm
-from django.http import HttpResponse
-from django.views.generic import View
-from .process import create_pdf 
-from django.template.loader import render_to_string
-from main_app import models
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.forms.models import inlineformset_factory
 import uuid
 import boto3
 
@@ -25,9 +17,7 @@ import boto3
 S3_BASE_URL='https://s3-us-west-2.amazonaws.com/'
 BUCKET='lostandhound'
 
-PhotoFormset = inlineformset_factory(
-  Pet, Photo, fields=('url',)
-)
+
 # Create your views here.
 
 # ------------------------- KL-todo apply auth routes ------------------------ #
@@ -41,8 +31,6 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-# def lostandhound_index(request):
-#   return render(request, 'pet/index.html')
 
 def signup(request):
     error_message = ''
@@ -61,71 +49,33 @@ def signup(request):
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
 
-
+@login_required
 def pets_index(request):
-  pets = Pet.objects.filter(user=request.user)
+  pets = Pet.objects.order_by('name')
   return render(request, 'pets/index.html', {'pets':pets})
-
-# def pets_detail(request, pet_id):
-#   pet = Pet.objects.get(id=pet_id)
-#   pet_form = PetForm()
-#   return render(request, 'pets/detail.html',{
-#     'pet': pet, 'pet_form': pet_form
-#   })
 
 def pets_detail(request, pet_id):
   pet = Pet.objects.get(id=pet_id)
   pet_form = PetForm()
   return render(request, 'pets/detail.html', {'pet': pet, 'pet_form': pet_form})
 
+def pets_create_photo(request, pet_id):
+  pet = Pet.objects.get(id=pet_id)
+  pet_form = PetForm()
+  return render(request, 'main_app/pet_form_photo.html', {'pet': pet, 'pet_form': pet_form})
+
 class PetList(LoginRequiredMixin, ListView):
   model = Pet
 
 class PetCreate(LoginRequiredMixin, CreateView):
-  model = Pet
-  fields = '__all__'
+    model = Pet
+    fields = '__all__'
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    success_url = '/pets/'
 
-  def get_context_data(self, **kwargs):
-    # need to overwrite to get_context_data to make sure formset is rendered
-    data = super().get_context_data(**kwargs)
-    if self.request.POST:
-      data["photos"] = PhotoFormset(self.request.POST, self.request.FILES.get('data', None), instance=self.object)
-      s3 = boto3.client('s3')
-      key = uuid.uuid4().hex[:6] + data[data.rfind('.'):]
-      print(key)
-    try:
-        s3.upload_fileobj(data, BUCKET, key)
-        url = f"{S3_BASE_URL}{BUCKET}/{key}"
-        photo = Photo(url=url)
-        photo.save()
-    except:
-      print(("Photo upload to S3 unsuccessful"))
-    else:
-      data["photos"] = PhotoFormset()
-    return data
-
-  def form_valid(self, form):
-    context = self.get_context_data()
-    photos = context["photos"]
-    self.object = form.save()
-    if photos.is_valid():
-      photos.instance = self.object
-      photos.save()
-    return super().form_valid(form)
-  def get_success_url(self):
-    return reverse("pets:list")
-
-  def form_valid(self, form):
-    context = self.get_context_data()
-    photo = context["photo"]
-    self.object = form.save()
-    if photo.is_valid():
-      photo.instance = self.object
-      photo.save()
-    # form.instance.user = self.request.user
-    return super().form_valid(form)
-
-class PetUpdate(UpdateView):
+class PetUpdate(LoginRequiredMixin, UpdateView):
   model = Pet
   fields = '__all__'
 
@@ -145,18 +95,18 @@ class GeneratePdf(LoginRequiredMixin, View):
          # rendering the template
         return HttpResponse(pdf, content_type='application/pdf')
 
-# def add_photo(request, pet_id):
-#   photo_file = request.FILES.get('photo-file', None)
-#   if photo_file:
-#     s3 = boto3.client('s3')
-#     key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-#     print(key)
-#     try:
-#         s3.upload_fileobj(photo_file, BUCKET, key)
-#         url = f"{S3_BASE_URL}{BUCKET}/{key}"
-#         photo = Photo(url=url)
-#         photo.save()
-#     except:
-#       print(("Photo upload to S3 unsuccessful"))
-#   return redirect('pets/create', pet_id='pet_id')
+@login_required
+def add_photo(request, pet_id):
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    try:
+        s3.upload_fileobj(photo_file, BUCKET, key)
+        url = f"{S3_BASE_URL}{BUCKET}/{key}"
+        photo = Photo(url=url, pet_id=pet_id)
+        photo.save()
+    except:
+      print(("Photo upload unsuccessful"))
+  return redirect('detail', pet_id=pet_id)
 
