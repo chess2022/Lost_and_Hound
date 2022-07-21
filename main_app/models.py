82cd75643e47f3e317db64305ac2840369e8fbbc
KELLY LUU <kellyluuu@gmail.com>
2022-07-20 21:39:29 -0500

    update user model with phone

--- main_app/models.py
+++ main_app/models.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import User
 from typing import TYPE_CHECKING
 from django.db import models
 from django.urls import reverse
+
 # Create your models here.
+
 
 class Pet(models.Model):
   class Meta:
