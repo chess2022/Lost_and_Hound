faf4f794010162fb70ab7f106c9a84880df6eda9
chess2022 <lakeshore.hnh@gmail.com>
2022-07-27 13:12:57 -0700

    deploy to heroku 10

--- lostandhound/settings.py
+++ lostandhound/settings.py
@@ -16,7 +16,7 @@ env=environ.Env(
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY=env('SECRET_KEY')
-DEBUG=env('DEBUG')
+# DEBUG=env('DEBUG')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG=False
